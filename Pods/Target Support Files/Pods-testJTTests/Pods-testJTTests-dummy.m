f8ae3ce60e42942ad8c51fcf0aadbabf824869a4
Oussam <oussam.tlili@jobteaser.com>
2019-09-23 11:32:02 +0200

    add cocoapods

---
+++ Pods/Target Support Files/Pods-testJTTests/Pods-testJTTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_testJTTests : NSObject
+@end
+@implementation PodsDummy_Pods_testJTTests
+@end
---
+++ Pods/Target Support Files/Pods-testJTUITests/Pods-testJTUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_testJTUITests : NSObject
+@end
+@implementation PodsDummy_Pods_testJTUITests
+@end
